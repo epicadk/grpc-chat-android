1d941e52136521fd3a4ed9b655a584a9d70740d7
epicadk <adityakurkure@gmail.com>
2021-04-23 09:27:24 +0530

    Use viewBinding

--- app/src/main/java/com/example/grpc_chat_android/MainActivity.kt
+++ app/src/main/java/com/example/grpc_chat_android/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.grpc_chat_android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.grpc_chat_android.databinding.ActivityMainBinding
 import com.example.grpc_chat_android.models.ChatServiceGrpc
 import io.grpc.ManagedChannelBuilder
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
