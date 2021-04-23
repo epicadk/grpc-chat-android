fbe5a911d34a02525541110d2e9f2342d9d37b93
epicadk <adityakurkure@gmail.com>
2021-04-23 10:15:19 +0530

    Update ChatAdapter.kt

--- app/src/main/java/com/example/grpc_chat_android/ChatAdapter.kt
+++ app/src/main/java/com/example/grpc_chat_android/ChatAdapter.kt
@@ -1,4 +1,32 @@
 package com.example.grpc_chat_android
 
-class ChatAdapter {
+import android.icu.text.Transliterator
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.grpc_chat_android.databinding.ItemChatBinding
+import com.example.grpc_chat_android.databinding.ItemMessageBinding
+import com.example.grpc_chat_android.models.Chat
+import java.util.Date
+
+class ChatAdapter(private val chatList : List<Chat.Message>) : RecyclerView.Adapter<ChatAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding : ItemChatBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder  =
+            ViewHolder(ItemChatBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.chatTitle.text = chatList[position].sender
+        holder.binding.chatLast.text = chatList[position].sender
+        //holder.binding.messageTime.text = Date().time.toString()
+    }
+
+    override fun getItemCount() =
+            chatList.size
+
 }
