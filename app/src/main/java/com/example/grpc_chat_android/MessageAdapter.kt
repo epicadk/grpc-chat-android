7f9cb7a2c5080f6c214aff2a4d0c726960b83dae
epicadk <adityakurkure@gmail.com>
2021-04-23 09:53:31 +0530

    Update MessageAdapter.kt

--- app/src/main/java/com/example/grpc_chat_android/MessageAdapter.kt
+++ app/src/main/java/com/example/grpc_chat_android/MessageAdapter.kt
@@ -1,4 +1,29 @@
 package com.example.grpc_chat_android
 
-class MessageAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.grpc_chat_android.databinding.ItemMessageBinding
+import com.example.grpc_chat_android.models.Chat
+import java.util.Date
+
+class MessageAdapter(private val messageList : List<Chat.Message>) : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding : ItemMessageBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageAdapter.ViewHolder  =
+         ViewHolder(ItemMessageBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+
+
+
+    override fun onBindViewHolder(holder: MessageAdapter.ViewHolder, position: Int) {
+       holder.binding.messageBody.text = messageList[position].body
+       holder.binding.messageTime.text = Date().time.toString()
+    }
+
+    override fun getItemCount() =
+        messageList.size
+
 }
