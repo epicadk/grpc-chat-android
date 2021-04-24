0df78863c6ea125d4a584573aab1d273405a108e
epicadk <adityakurkure@gmail.com>
2021-04-24 16:56:23 +0530

    Create bav graph

---
+++ app/src/main/java/com/example/grpc_chat_android/view/fragments/AddUserFragment.kt
@@ -0,0 +1,26 @@
+package com.example.grpc_chat_android.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.grpc_chat_android.databinding.FragmentAddUserBinding
+
+class AddUserFragment : DialogFragment() {
+    private var _binding : FragmentAddUserBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAddUserBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
