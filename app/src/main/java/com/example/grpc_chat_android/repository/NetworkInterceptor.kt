78858e76de9cd2dff4c576af3688ec6dbf071b2d
Hersh Kumar <56927116+unc0ded@users.noreply.github.com>
2021-05-24 13:54:15 +0530

    bug fix

--- app/src/main/java/com/example/grpc_chat_android/repository/NetworkInterceptor.kt
+++ app/src/main/java/com/example/grpc_chat_android/repository/NetworkInterceptor.kt
@@ -11,23 +11,26 @@ import io.grpc.MethodDescriptor
 import javax.inject.Inject
 import kotlinx.coroutines.runBlocking
 
-class NetworkInterceptor @Inject constructor(private val preferenceManager: PreferenceManager) : ClientInterceptor {
+class NetworkInterceptor @Inject constructor(private val preferenceManager: PreferenceManager) :
+    ClientInterceptor {
     override fun <ReqT : Any?, RespT : Any?> interceptCall(
         method: MethodDescriptor<ReqT, RespT>?,
         callOptions: CallOptions?,
         next: Channel?
     ): ClientCall<ReqT, RespT> {
-        return object : ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(next!!.newCall(
-            method,
-            callOptions
-        )) {
+        return object : ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(
+            next!!.newCall(
+                method,
+                callOptions
+            )
+        ) {
             override fun start(responseListener: Listener<RespT>?, headers: Metadata?) {
                 val metadata = Metadata()
                 val token = runBlocking {
                     preferenceManager.getToken()
                 }
                 val key: Metadata.Key<String> =
-                    Metadata.Key.of("Auth", Metadata.ASCII_STRING_MARSHALLER)
+                    Metadata.Key.of("authorization", Metadata.ASCII_STRING_MARSHALLER)
                 metadata.put(key, token)
                 headers?.merge(metadata)
                 super.start(responseListener, headers)
